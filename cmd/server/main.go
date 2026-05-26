54a1ce86923ac636ceeb1ae92f625c984e86eecb
Matheus Moreira <matheus.machadomoreira@eye.security>
2026-05-26 21:08:37 +0200

    Finished CH2: Message Brokers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	connString := "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(connString)
+	if err != nil {
+		log.Fatalf("failed to connect to RabbitMQ server: %s", err)
+	}
+	defer conn.Close()
+
+	log.Println("connected successfully to RabitMQ server")
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	<-ctx.Done()
+	fmt.Println("shutting down")
 }
